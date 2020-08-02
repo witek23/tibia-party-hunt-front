f2b465c48cc221b5c30c9b7dd6d75e2bfd338743
witek23 <garkstal_shop2@vp.pl>
2020-08-02 20:12:16 +0200

    Added WelcomePage Image

--- src/components/emptyComponents/welcome/welcomePage.jsx
+++ src/components/emptyComponents/welcome/welcomePage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 const WelcomePage = () => {
   return (
-    <div>
+    <div className="main">
       <h3>Welcome to super page by Witek</h3>
       <button className="btn btn-info">Go to the website</button>
     </div>
