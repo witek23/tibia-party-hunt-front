a0b6d28ff893f6a9db84ec877bf72ed9e5c767b5
witek23 <garkstal_shop2@vp.pl>
2020-08-03 19:46:04 +0200

    Changed env name with prefix REACT_APP

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
+console.log("API URL", process.env);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
